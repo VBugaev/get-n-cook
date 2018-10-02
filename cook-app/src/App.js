5e378f5688ed3a73f7861f713346aa0c8a3df422
Vladimir Bugaev <vladimir_bugaev@epam.com>
2018-10-02 12:09:24 +0400

    initial commit

---
+++ cook-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>Get'n'Cook app</header>
+      </div>
+    );
+  }
+}
+
+export default App;
