2f5ba968999973ab17bd82d1bef0b9500ba86a25
Vladimir Bugaev <vladimir_bugaev@epam.com>
2018-12-10 21:54:10 +0400

    ingredients dal provided

---
+++ server/components/ingredients/dal.js
@@ -0,0 +1,46 @@
+let sql = require('mssql');
+let pool = require('../../db.js');
+
+const getAllIngredients = async (filter) => {
+    try {
+        let connectedPool = await pool;
+        const result = await connectedPool.request()
+                            .execute('GetAllIngredients');
+        if (filter) {
+            return result.recordset.filter(ingredient => ingredient.Title.includes(filter));
+        }
+        return result.recordset;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const getIngredientById = async (id) => {
+    try {
+        let connectedPool = await pool;
+        const result = await connectedPool.request()
+                            .input('IngredientId', sql.UniqueIdentifier, id)
+                            .execute('GetIngredientById');
+        return result.recordset[0];
+    } catch (error) {
+        throw error;
+    }
+};
+
+const createIngredient = async (title) => {
+    try {
+        let connectedPool = await pool;
+        const result = await connectedPool.request()
+                            .input('Title', sql.NVarChar(50), title)
+                            .execute('CreateIngredient');
+        return result.recordset[0];
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = {
+    getAllIngredients,
+    getIngredientById,
+    createIngredient
+};
