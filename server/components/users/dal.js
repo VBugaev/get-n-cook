86d6421ca73f671e04911c83cbc7af319179a201
Vladimir Bugaev <vladimirBugaevv@gmail.com>
2018-12-10 09:17:34 +0400

    usersDal provided

--- server/components/users/dal.js
+++ server/components/users/dal.js
@@ -16,28 +16,124 @@ const getAllUsers = () => {
 };
 
 const getUserByEmail = (email) => {
-
+    return pool.then(pool => {
+        return pool.request()
+            .input('Email', sql.NVarChar(sql.MAX), email)
+            .execute('GetUserByEmail');
+    }).then(result => {
+        return result.recordset[0];
+    })
+    .catch(err => {
+        sql.close();
+        throw err;
+    })
 };
 
 const getUserById = (id) => {
-
+    return pool.then(pool => {
+        return pool.request()
+            .input('UserId', sql.UniqueIdentifier, id)
+            .execute('GetUserById')
+    })
+    .then(result => {
+        return result.recordset[0];
+    })
+    .catch(err => {
+        throw err;
+    });
 };
 
 const createUser = (userData) => {
-
+    return pool.then(pool => {
+        return pool.request()
+            .input('Login', sql.NVarChar(50), userData.login)
+            .input('Email', sql.NVarChar(sql.MAX), userData.email)
+            .input('Password', sql.NVarChar(sql.MAX), userData.password)
+            .input('RoleId', sql.UniqueIdentifier, userData.roleId)
+            .input('Name', sql.NVarChar(50), userData.name)
+            .input('Surname', sql.NVarChar(50), userData.surname)
+            .input('AboutSection', sql.NVarChar(sql.MAX), userData.about)
+            .execute('CreateUser')
+    })
+    .then(result => {
+        return result.recordset[0];
+    })
+    .catch(err => {
+        throw err;
+    });
 };
 
 const updateUser = (userData) => {
-
-}
+    return pool.then(pool => {
+        return pool.request()
+            .input('Id', sql.UniqueIdentifier, userData.id)
+            .input('Login', sql.NVarChar(50), userData.login)
+            .input('Email', sql.NVarChar(sql.MAX), userData.email)
+            .input('RoleId', sql.UniqueIdentifier, userData.roleId)
+            .input('Name', sql.NVarChar(50), userData.name)
+            .input('Surname', sql.NVarChar(50), userData.surname)
+            .input('AboutSection', sql.NVarChar(sql.MAX), userData.about)
+            .execute('UpdateUserById')
+    })
+    .then(result => {
+        return result.recordset[0];
+    })
+    .catch(err => {
+        throw err;
+    });
+};
 
 const deleteUser = (id) => {
-
+    return pool.then(pool => {
+        return pool.request()
+            .input('UserId', sql.UniqueIdentifier, id)
+            .execute('DeleteUserById')
+    })
+    .then(() => {
+        return true;
+    })
+    .catch(err => {
+        throw err;
+    });
 };
+
+const setPassword = (setData) => {
+    return pool.then(pool => {
+        return pool.request()
+            .input('UserId', sql.UniqueIdentifier, setData.userId)
+            .input('Password', sql.NVarChar(sql.MAX), setData.password)
+            .execute('SetPassword')
+    })
+    .then(() => {
+        return true;
+    })
+    .catch(err => {
+        throw err;
+    });
+};
+
+const updatePassword = (updData) => {
+    return pool.then(pool => {
+        return pool.request()
+            .input('UserId', sql.UniqueIdentifier, updData.userId)
+            .input('Password', sql.NVarChar(sql.MAX), updData.password)
+            .execute('UpdatePassword')
+    })
+    .then(() => {
+        return true;
+    })
+    .catch(err => {
+        throw err;
+    });
+}
 
 module.exports = {
     getAllUsers,
     getUserByEmail,
     getUserById,
-    createUser
+    createUser,
+    updateUser,
+    deleteUser,
+    setPassword,
+    updatePassword
 };
